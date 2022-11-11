379d30e48bc83f8d631d4ef3155129d4af1b867f
minha0817 <minhakk24@gmail.com>
2022-11-11 12:07:07 -0800

    add websocket

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,6 +10,8 @@ import {
   getMostPopularDay,
   getInterviewsPerDay
 } from "helpers/selectors";
+import { setInterview } from "helpers/reducers";
+
 
 const data = [
   {
@@ -64,12 +66,29 @@ class Dashboard extends Component {
       });
     });
 
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+    
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
+
   }
 
   componentDidUpdate(previousProps, previousState) {
     if (previousState.focused !== this.state.focused) {
       localStorage.setItem("focused", JSON.stringify(this.state.focused));
     }
+  }
+
+
+  componentWillUnmount() {
+    this.socket.close();
   }
 
 
