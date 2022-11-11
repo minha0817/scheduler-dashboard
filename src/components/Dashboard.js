8b562d225cdfc41e78fd520d17d0912002cd2965
minha0817 <minhakk24@gmail.com>
2022-11-10 21:40:07 -0800

    update localStorage

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -30,25 +30,52 @@ const data = [
 class Dashboard extends Component {
 
   state = {
-    loading: false
+    loading: false,
+    focused: null
   }
 
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  
+  selectPanel(id) {
+    this.setState((previousState) => ({
+
+      focused: (this.state.focused !== null ? null : id)
+
+    }));
+  } 
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if(this.state.loading){
       return <Loading />
     }
 
-    const panels = data.map(panel => (
-      <Panel  
-        key = {panel.id}
-        id = {panel.id}
-        label={panel.label} 
-        value={panel.value}  
-      />
-    ))
+    const panels = (this.state.focused ?  data.filter(panel => this.state.focused === panel.id)   : data)
+      .map(panel => (
+        <Panel  
+          key = {panel.id}
+          id = {panel.id}
+          label={panel.label} 
+          value={panel.value} 
+          onSelect={event => this.selectPanel(panel.id)}
+        />
+      ))
 
     return <main className={dashboardClasses}>{panels}</main>
 
